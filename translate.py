dd7c5e1523adb55de5fe1522ac5ef25182f6b383
alperunlu07 <52657487+alperunlu07@users.noreply.github.com>
2019-09-07 20:18:34 +0300

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,27 +1,64 @@
+#!/usr/bin/python3
 import os
 import pyperclip
 import time
 import requests
 from bs4 import BeautifulSoup
+from win10toast import ToastNotifier
 
-def notify(title, text):
-    os.system("""
-              osascript -e 'display notification "{}" with title "{}"'
-              """.format(text, title))
+
+def notify(title, means):
+       output = ""
+       if(os.name == "posix"):
+              for i in range(len(means)):
+                     output += str(i+1)+": "
+                     output += means[i]
+                     output += "\t"
+                     if(i>5):
+                            break
+              os.system("""
+                     osascript -e 'display notification "{}" with title "{}"'
+                     """.format(output, title))
+   
+       if(os.name == "nt"):
+              for i in range(len(means)):
+                     output += str(i+1)+": "
+                     output += means[i]
+                     output += "\n"
+                     if(i>10):
+                            break
+              toaster = ToastNotifier()
+              toaster.show_toast(title,output)
 
 def translate(word):
        urlx = "https://tureng.com/tr/turkce-ingilizce/"
        r = requests.get(str(urlx+word),allow_redirects=False)
        context = r.content
        soup = BeautifulSoup(context, "html.parser")
+       mean = []              
        for i in soup.find_all("td",{"class":"tr ts"}):
-              return str(i.text)
-       
+              mean.append(i.text)       
+       return mean
+#print(translate("book"))
+def anlam(means):
+       output = ""
+       for i in range(len(means)):
+              output += str(i+1)+": "
+              output += means[i]
+              output += "\n"
+              if(i>5):
+                     break
+       return output
 
 copy = pyperclip.paste()
-while(true):
-
+while(True):
        if(copy!=pyperclip.paste()):
               copy = pyperclip.paste()
-              notify(copy, translate(copy))
+              notify(copy, translate(copy.lower()))
+              
        time.sleep(1)
+
+
+
+
+
